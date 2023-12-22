02656ae4a4d97ea1a874b181581b9933267a74c9
worldneedwrok <1837563622@qq.com>
2023-12-22 10:24:51 +0800

    tuxingfenxi

---
+++ WindowsFormsApp7/WindowsFormsApp7/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace WindowsFormsApp7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(1, 185);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(1527, 493);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(282, 25);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(198, 81);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(1533, 837);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ WindowsFormsApp7/WindowsFormsApp7/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace WindowsFormsApp7
+{
+    public partial class Form1 : Form
+    {
+        private List<double> voltages = new List<double>();
+        public Form1()
+        {
+            InitializeComponent();
+      
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string filePath = @"test.txt";
+
+            // 读取文件中包含 "CellVs:" 的行，并提取数据
+            List<double> dataPoints = new List<double>();
+            using (StreamReader sr = new StreamReader(filePath))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    int index = line.IndexOf("CellVs:");
+                    if (index >= 0)
+                    {
+                        string dataString = line.Substring(index + 7).Trim();
+                        double data = double.Parse(dataString);
+                        dataPoints.Add(data);
+                    }
+                }
+            }
+            dataPoints = dataPoints.Select(d => Math.Round(d, 3)).ToList();
+
+            // 统计数据点的频率
+            Dictionary<double, int> frequency = new Dictionary<double, int>();
+            foreach (double dataPoint in dataPoints)
+            {
+                if (frequency.ContainsKey(dataPoint))
+                {
+                    frequency[dataPoint]++;
+                }
+                else
+                {
+                    frequency[dataPoint] = 1;
+                }
+            }
+
+            // 清空绘图区域
+            chart1.Series.Clear();
+
+            // 添加柱状图系列
+            Series series = new Series("Frequency", 0);
+            series.ChartType = SeriesChartType.Column;
+            foreach (var kvp in frequency.OrderBy(k => k.Key))
+            {
+                series.Points.AddXY(kvp.Key, kvp.Value);
+            }
+            chart1.Series.Add(series);
+
+            // 设置 X 轴和 Y 轴的标签
+            chart1.ChartAreas[0].AxisX.Title = "Data Points";
+            chart1.ChartAreas[0].AxisY.Title = "Frequency";
+
+            // 自动调整 Y 轴的最大值和最小值
+            chart1.ChartAreas[0].RecalculateAxesScale();
+            chart1.ChartAreas[0].AxisX.LabelStyle.Format = "0.000";
+            chart1.ChartAreas[0].AxisX.Interval = 0.01; // 设置刻度间隔为1
+            chart1.ChartAreas[0].AxisX.Minimum = 3.40; // 设置最小值
+            chart1.ChartAreas[0].AxisX.Maximum = 3.62; // 设置最大值
+
+            // 显示图形
+            chart1.Visible = true;
+        }
+
+    }
+}
